bf0669b02037a5a8e2d75bbbf916c3b98a913d3d
Nodirboooy <nodirbekusmanov991@gmail.com>
2024-07-10 17:33:55 +0500

    added my Ekub method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        
+    }
+
+    public static int Ekub(int a, int b){
+        while(a!=b){
+            if(a>b){
+                a=a-b;
+            } else{
+                a= b-a;
+            }
+        }
+        return a;
     }
 }

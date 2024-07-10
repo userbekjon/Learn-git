657a06fc4b2b693250ea952516ca8615f25c255b
Davron <ergashevdavron694@gmail.com>
2024-07-10 17:33:18 +0500

    Add primeNumber method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        evenNumbers(10);
     }
+
+    public static void evenNumbers(int num) {
+        int count = 0;
+        for (int i = 2; i < num; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if(i % j == 0){
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0){
+                System.out.println(i);
+            }
+                count = 0;
+        }
+    }
+
 }
